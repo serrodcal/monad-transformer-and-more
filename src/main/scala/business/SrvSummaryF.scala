50b9b1494b9a84a5eb4e2ea49a5357f90f5ac7c1
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-11-27 16:01:59 +0100

    With Scala :)

--- src/main/scala/business/SrvSummaryF.scala
+++ src/main/scala/business/SrvSummaryF.scala
@@ -1,10 +1,11 @@
 package com.logicaalternativa.monadtransformerandmore
 package business
 
+import java.util.Optional
+
 import monad._
 import bean._
 import service._
-
 import monad.syntax.Implicits._
 
 trait SrvSummaryF[E,P[_]] {
@@ -19,8 +20,14 @@ trait SrvSummaryF[E,P[_]] {
   val srvAuthor : ServiceAuthorF[E,P]
   
   def getSummary( idBook: Int) : P[Summary] = {
-    
-     ???
+
+     for {
+       book <- srvBook.getBook(idBook)
+       sales <- srvSales.getSales(idBook)
+
+     }yield(
+       new Summary(book, null, Optional.of(sales), null)
+     )
     
   } 
   
