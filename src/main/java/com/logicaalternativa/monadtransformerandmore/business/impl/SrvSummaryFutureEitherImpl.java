5849963137c091bf2bb624d13f6699b3b5f01504
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-11-27 14:28:05 +0100

    Classic implementation

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
@@ -1,7 +1,12 @@
 package com.logicaalternativa.monadtransformerandmore.business.impl;
 
+import akka.dispatch.OnComplete;
+import com.logicaalternativa.monadtransformerandmore.bean.Book;
+import com.logicaalternativa.monadtransformerandmore.bean.Sales;
+import scala.Function1;
 import scala.concurrent.Future;
 import scala.concurrent.Promise;
+import scala.runtime.BoxedUnit;
 import scala.util.Either;
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
@@ -15,6 +20,9 @@ import com.logicaalternativa.monadtransformerandmore.service.future.ServiceAutho
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceBookFutEither;
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceChapterFutEither;
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceSalesFutEither;
+import scala.util.Right;
+
+import java.util.Optional;
 
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
 import static com.logicaalternativa.monadtransformerandmore.bean.Java8.*;
@@ -44,7 +52,46 @@ public class SrvSummaryFutureEitherImpl implements SrvSummaryFutureEither<Error>
 
 	@Override
 	public Future<Either<Error, Summary>> getSummary(Integer idBook) {
-		
+
+		//Primero creamos una promesa del resultado, en este caso, de un Either<Error, Summary>
+
+		Promise<Either<Error, Summary>> promise = Future.promise();
+
+		Future<Either<Error, Book>> book = srvBook.getBook(idBook);
+		Future<Either<Error, Sales>> sales = srvSales.getSales(idBook);
+
+		book.onComplete(
+
+				new OnComplete<Either<Error, Book>>() {
+					
+					@Override
+					public void onComplete(Throwable failure, Either<Error, Book> success) throws Throwable {
+						if (success != null) {
+							if (success.isRight()) {
+								final Book book = success.right().get();
+
+								sales.onComplete(new OnComplete<Either<Error, Sales>>() {
+									@Override
+									public void onComplete(Throwable failure, Either<Error, Sales> success) throws Throwable {
+										if (success != null) {
+											if (success.isRight()) {
+												final Sales sales = success.right().get();
+
+												final Summary summary = new Summary(book, null, Optional.of(Sales), null);
+
+												promise.success(new Right<Error, Summary>(summary));
+											}
+										}
+									}
+
+								});
+							}
+						}
+					}
+				}
+
+		);
+
 		return $_notYetImpl();
 	}
 
